c690ba139932d8a663dd7c4014a30aeadf0c511a
CaioDetore <caio.detore@sou.unifeob.edu.br>
2025-02-01 14:51:26 -0300

    feat: interface tabela de transações

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,6 +1,10 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
-import { TransactionsContainer } from "./styles";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
@@ -8,7 +12,28 @@ export function Transactions() {
       <Header />
       <Summary />
       <TransactionsContainer>
-        <h1>Transactions</h1>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento do site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.800,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>12/07/2025</td>
+            </tr>
+            <tr>
+              <td width="50%">Pão</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 12.800,00
+                </PriceHighlight>
+              </td>
+              <td>Compra</td>
+              <td>12/07/2025</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
       </TransactionsContainer>
     </div>
   );
